122fd3d5aa1fcc2e67861a51a0cb42e9b173065f
sofy <bedlusofi@gmail.com>
2023-06-26 21:22:11 +0000

    fix-eslint

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,6 +1,5 @@
 export function taskFirst() {
 	  const task = 'I prefer const when I can.';
-
 	  return task;
 }
 
@@ -11,6 +10,5 @@ export function getLast() {
 export function taskNext() {
 	  let combination = 'But sometimes let';
 	  combination += getLast();
-
 	  return combination;
 }
