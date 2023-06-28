3615dba7a1348586374763c99d5ea3b10198aee1
sofy <bedlusofi@gmail.com>
2023-06-28 10:07:58 +0000

    task-3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  uploadPhoto()
+    .then((data) => data.body)
+    .then((value) => {
+      createUser().then((obj) => {
+        console.log(`${value} ${obj.firstName} ${obj.lastName}`);
+      });
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
