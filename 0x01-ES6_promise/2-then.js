77120389ee5fb30831224beb92c7ae12a2f3a2c7
sofy <bedlusofi@gmail.com>
2023-06-28 06:42:53 +0000

    update-the-catch-method

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -4,5 +4,8 @@ export default function handleResponseFromAPI(promise) {
     console.log('Got a response from the API');
     return data;
   },
-  () => new Error());
+  () => {
+    console.log('Got a response from the API');
+    return new Error();
+  });
 }
