bd433b8e41bd2566cef294247219a62fffff2f31
sofy <bedlusofi@gmail.com>
2023-06-29 17:37:05 +0000

    task-4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,43 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value === 'number') {
+      this._amount = value;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (value instanceof Currency) {
+      this._currency = value;
+    } else {
+      throw new TypeError('currency must be instance of Currency');
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount === 'number' && typeof conversionRate === 'number') {
+      return amount * conversionRate;
+    }
+    throw new TypeError('amount and conversionRate must be a number');
+  }
+}
