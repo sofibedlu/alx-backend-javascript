2ba96474a68e81b0d8b64ac7433ede4a0519b73d
sofy <bedlusofi@gmail.com>
2023-06-30 20:18:01 +0000

    flat-method-updated

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,5 +1,5 @@
 export default function createIteratorObject(report) {
   const obj = report.allEmployees;
-  const result = Object.values(obj).flatMap((namesArray) => namesArray);
+  const result = Object.values(obj).flat();
   return result;
 }
