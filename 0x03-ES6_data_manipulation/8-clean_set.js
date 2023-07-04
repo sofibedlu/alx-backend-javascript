5e633d325fa92b9a2a5e515f71d3123b268d7c37
sofy <bedlusofi@gmail.com>
2023-07-04 14:59:18 +0000

    update-for-empty-string

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,5 @@
 export default function cleanSet(set, startString) {
   const newStr = [];
-  if (startString === '') {
-    return '';
-  }
   for (const elem of set) {
     if (elem.startsWith(startString)) {
       newStr.push(elem.slice(startString.length));
