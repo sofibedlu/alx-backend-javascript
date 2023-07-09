16b12444d62e64a2d5a025f52fada8bd9c68b679
sofy <bedlusofi@gmail.com>
2023-07-09 15:55:32 +0000

    task-7

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -57,3 +57,15 @@ function executeWork(employee: Director | Teacher) {
 	}
 }
 
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+	if (todayClass === "Math") {
+		return "Teaching Math";
+	} else if (todayClass === "History") {
+		return "Teaching History";
+	} else {
+		throw new Error("Invalid subject");
+	}
+}
+
