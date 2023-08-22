b4def213e8eb65c99af9acdb6d394957d34aeabe
sofy <bedlusofi@gmail.com>
2023-08-22 15:35:59 +0300

    refactor

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,20 +1,13 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.once('line', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
-  rl.close();
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
-rl.on('close', () => {
-  if (!process.stdin.isTTY) {
-    console.log('This important software is now closing');
-  }
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
