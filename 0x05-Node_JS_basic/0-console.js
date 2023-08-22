65f165af8527f54ad74338735102ce51c086d897
sofy <bedlusofi@gmail.com>
2023-08-22 14:31:15 +0300

    add-docs

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,3 +1,8 @@
+/**
+ * Display a message in the standard output.
+ * @param {string} message - The message to be displayed.
+ * @returns {void}
+ */
 const displayMessage = (message) => {
   process.stdout.write(`${message}\n`);
 };
