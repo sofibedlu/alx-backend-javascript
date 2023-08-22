91b9ce1b530d175d4b4cb08dcd8b055f41e97aa8
sofy <bedlusofi@gmail.com>
2023-08-22 16:57:42 +0300

    update

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,33 +1,35 @@
 const fs = require('fs');
 
 function countStudents (path) {
-  try {
-    const data = fs.readFileSync(path, 'utf-8');
-    const lines = data.split('\n').filter(line => line.trim() !== '');
-
-    const headers = lines[0].split(',');
-
-    const fieldCounts = {};
-    for (let i = 1; i < lines.length; i++) {
-      const values = lines[i].split(',');
-      const field = values[headers.indexOf('field')];
-
-      if (!fieldCounts[field]) {
-        fieldCounts[field] = {
-          count: 0,
-          names: []
-        };
-      }
-      fieldCounts[field].count++;
-      fieldCounts[field].names.push(values[headers.indexOf('firstname')]);
-    }
-
-    console.log(`Number of students: ${lines.length - 1}`);
-    for (const field in fieldCounts) {
-      console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
-    }
-  } catch (error) {
+  if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
+  }
+  if (!fs.statSync(path).isFile()) {
+    throw new Error('Cannot load the database');
+  }
+  const data = fs.readFileSync(path, 'utf-8');
+  const lines = data.split('\n').filter(line => line.trim() !== '');
+
+  const headers = lines[0].split(',');
+
+  const fieldCounts = {};
+  for (let i = 1; i < lines.length; i++) {
+    const values = lines[i].split(',');
+    const field = values[headers.indexOf('field')];
+
+    if (!fieldCounts[field]) {
+      fieldCounts[field] = {
+        count: 0,
+        names: []
+      };
+    }
+    fieldCounts[field].count++;
+    fieldCounts[field].names.push(values[headers.indexOf('firstname')]);
+  }
+
+  console.log(`Number of students: ${lines.length - 1}`);
+  for (const field in fieldCounts) {
+    console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
   }
 }
 
