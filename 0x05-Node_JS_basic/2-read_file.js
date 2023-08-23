c862514e39d644aee07d0ea204f44412c24694d8
sofy <bedlusofi@gmail.com>
2023-08-23 13:22:28 +0300

    fix-lint

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function countStudents (path) {
+function countStudents(path) {
   if (!fs.existsSync(path)) {
     throw new Error('Cannot load the database');
   }
@@ -8,28 +8,30 @@ function countStudents (path) {
     throw new Error('Cannot load the database');
   }
   const data = fs.readFileSync(path, 'utf-8');
-  const lines = data.split('\n').filter(line => line.trim() !== '');
+  const lines = data.split('\n').filter((line) => line.trim() !== '');
 
   const headers = lines[0].split(',');
 
   const fieldCounts = {};
-  for (let i = 1; i < lines.length; i++) {
+  for (let i = 1; i < lines.length; i += 1) {
     const values = lines[i].split(',');
     const field = values[headers.indexOf('field')];
 
     if (!fieldCounts[field]) {
       fieldCounts[field] = {
         count: 0,
-        names: []
+        names: [],
       };
     }
-    fieldCounts[field].count++;
+    fieldCounts[field].count += 1;
     fieldCounts[field].names.push(values[headers.indexOf('firstname')]);
   }
 
   console.log(`Number of students: ${lines.length - 1}`);
   for (const field in fieldCounts) {
-    console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
+    if (Object.prototype.hasOwnProperty.call(fieldCounts, field)) {
+      console.log(`Number of students in ${field}: ${fieldCounts[field].count}. List: ${fieldCounts[field].names.join(', ')}`);
+    }
   }
 }
 
