4905a16cbc32d953628fa074890622cf550913e0
sofy <bedlusofi@gmail.com>
2023-08-25 10:51:54 +0300

    update-server

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import routes from './routes/index';
 
 const app = express();
 const port = 1245;
+
+app.use(bodyParser.json());
 
 app.use('/', routes);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
+
+export default app;
