2bc266e67c6a9a566c68becef74329c78ca1376b
sofy <bedlusofi@gmail.com>
2023-08-26 10:27:34 +0300

    lint

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,11 +1,8 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import routes from './routes/index';
 
 const app = express();
 const port = 1245;
-
-app.use(bodyParser.json());
 
 app.use('/', routes);
 
