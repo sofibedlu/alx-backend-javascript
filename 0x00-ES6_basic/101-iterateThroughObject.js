dc978436d78b5d71ed472d86d3c3e3f8f61b0009
sofy <bedlusofi@gmail.com>
2023-09-25 12:22:49 +0300

    simplify

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,12 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let nameStr = '';
-  let count = 0;
-  for (const name of reportWithIterator) {
-    nameStr += name;
-    if (count !== reportWithIterator.length - 1) {
-      nameStr += ' | ';
-    }
-    count += 1;
-  }
-  return nameStr;
+  return reportWithIterator.join('|');
 }
