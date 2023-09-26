4da73261b2ee4a79420055ff2fe41bbb92934e43
sofy <bedlusofi@gmail.com>
2023-09-26 09:16:46 +0300

    update-func

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,22 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 async function handleProfileSignup(firstName, lastName, fileName) {
-  // Create an array of promises
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  // Use Promise.allSettled to wait for all promises to settle
-  const results = await Promise.allSettled(promises);
-
-  // Map the results to the desired format
-  const formattedResults = results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
-
-  return formattedResults;
+  const results = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    results.push({ status: 'fulfilled', value: user });
+    await uploadPhoto(fileName);
+  } catch (err) {
+    results.push({ status: 'rejected', value: err });
+  }
+  return results;
 }
 
 export default handleProfileSignup;
